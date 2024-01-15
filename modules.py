e3a7ded8e538d51b88378b6253bf48e81c9946b9
Mujkaz <h23vilmu@du.se>
2024-01-15 14:39:34 +0100

    Changes by William

--- modules.py
+++ modules.py
@@ -1,5 +1,19 @@
 import random
 
-print("Ange ett nummer!")
+print("Gissa vilket nummer jag tänker på mellan 1-60!")
 
 number = random.randint(1,60)
+
+guess = -1
+
+while guess != number:
+    guess_string = input("Vilket nummer gissar du på? ")
+    guess = int(guess_string)
+
+    if guess < number:
+        print("För lågt, försök igen!")
+    elif guess > number:
+        print("För högt, försök igen!")
+
+print(f"Du lyckades gissa rätt! Jag tänkte på {number}!")1212
+
