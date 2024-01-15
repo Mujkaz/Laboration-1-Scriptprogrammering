ed156a86c8be5797baa49a234f204e68f199072e
Mujkaz <h23vilmu@du.se>
2024-01-15 15:43:46 +0100

    Test

--- modules.py
+++ modules.py
@@ -1,19 +1,25 @@
 import random
 
+" Hitta och skriva ut alla tal mellan 1 – 1600 som är delbara med två valda heltal "
+
+" Användaren ska gissa vilken siffra. "
 print("Gissa vilket nummer jag tänker på mellan 1-60!")
 
 number = random.randint(1,60)
 
 guess = -1
-
+tries = 1
 while guess != number:
     guess_string = input("Vilket nummer gissar du på? ")
     guess = int(guess_string)
 
     if guess < number:
-        print("För lågt, försök igen!")
+        tries = tries + 1
+        print("Du svarade fel, försök igen. Det var för lågt.")
     elif guess > number:
-        print("För högt, försök igen!")
+        tries = tries + 1
+        print("Du svarade fel, försök igen. Det var för högt.")
 
-print(f"Du lyckades gissa rätt! Jag tänkte på {number}!")1212
+print(f"Du lyckades gissa rätt! Jag tänkte på {number}!")
+print(f"Det tog dig så här många gissningar {tries}!")
 
